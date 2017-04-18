8d55240a5f1009d8d45ff65440537e57bf30ea67
humenspider7 <iamyusuf6@aol.com>
2017-04-18 12:28:05 -0400

    added sound manager

---
+++ KeepItUpCorrect/KeepItUpCorrect/KeepItUpCorrect/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace KeepItUpCorrect
+{
+    class SoundManager
+    {
+        private static SoundEffect jump;
+
+
+        public static void Initialize(ContentManager content)
+        {
+            jump = content.Load<SoundEffect>(@"Sounds\jump");
+        }
+        public static void playJump()
+        {
+            jump.Play();
+        }
+
+
+
+
+    }
+}
